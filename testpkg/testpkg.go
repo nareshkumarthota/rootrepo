8702822f73f063875e65177905173ae1098f0865
nthota <nthota@tibco.com>
2019-04-24 13:59:57 +0530

    added testpkg content

--- testpkg/testpkg.go
+++ testpkg/testpkg.go
@@ -2,7 +2,12 @@ package testpkg
 
 import "fmt"
 
+func init() {
+	Display()
+}
+
 // Display display method
 func Display() {
 	fmt.Println("test package repo display method()")
+	fmt.Println("new method added")
 }
